2a8726e0e6169d103232f2d9a071c51b1ad23bf5
caio cascaes <caio cascaes>
2017-03-09 20:36:43 -0300

    Aula de Xamarin

---
+++ MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace Login
+{
+    [Activity(Label = "Login", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            Android.Widget.Button botaoSignEmail = FindViewById<Android.Widget.Button>(Resource.Id.button2);
+            botaoSignEmail.Click += delegate
+            {
+                var transaction = FragmentManager.BeginTransaction();
+                var dialogFragment = new Fragment1();
+                dialogFragment.Show(transaction, "dialog_fragment");
+                //StartActivity(typeof(Activity1));
+            };
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
